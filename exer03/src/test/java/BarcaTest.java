8bedfc63d847dfc4b4df7a498965a902b616e3bd
MariaContu <mariaescontu@gmail.com>
2023-10-01 15:26:36 -0300

    tests done

--- exer03/src/test/java/BarcaTest.java
+++ exer03/src/test/java/BarcaTest.java
@@ -1,66 +1,76 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.bcopstein.Barca;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-
 public class BarcaTest {
-    private Barca barca;
+
+    private Barca barca = new Barca();
+
     @BeforeEach
     void setUp(){
         // Crie uma instância da classe Barca
         barca = new Barca();// Crie uma instância da classe Barca
     }
-    // Casos de teste definidos na letra "a"
+
+    //testes por particionamento
+
     @Test
-    public void testUltimaFileira() {
-        assertEquals(0, barca.ocupaLugar("F61A01")); // Fileira no intervalo permitido (off-point)
-        assertEquals(0, barca.ocupaLugar("F60A01")); // Fileira fora do intervalo permitido (on-point)
-        assertEquals(0, barca.ocupaLugar("F02A10")); // testOcupado()
+    public void testaPartAssentoInvalido()  {
+        assertEquals(0, barca.ocupaLugar("F1A01"));
+        assertEquals(0, barca.ocupaLugar("F01A1"));
+        assertEquals(0, barca.ocupaLugar("F67A15"));
+        assertEquals(0, barca.ocupaLugar("F37A26"));
     }
 
     @Test
-    public void testUltimoAssento(){
-        assertEquals(0, barca.ocupaLugar("F02A61")); // off-point
-        assertEquals(0, barca.ocupaLugar("F02A60")); // on-point
-        assertEquals(0, barca.ocupaLugar("F60A01")); // testUltimaFileira()
-        assertEquals(0, barca.ocupaLugar("F39A10")); // testDistribuicaoDePeso()
+    public void testaPartAssentoOcupado()  {
+        //ocupar assentos com metodo para teste
+        barca.ocupaLugarSemVerificacao(10, 5);
+        barca.ocupaLugarSemVerificacao(23, 13);
+        barca.ocupaLugarSemVerificacao(1, 5);
+        barca.ocupaLugarSemVerificacao(54, 10);
+
+        assertEquals(1, barca.ocupaLugar("F10A05"));
+        assertEquals(1, barca.ocupaLugar("F23A13"));
+        assertEquals(1, barca.ocupaLugar("F01A05"));
+        assertEquals(1, barca.ocupaLugar("F54A10"));
     }
 
     @Test
-    public void testFormato(){
-        assertEquals(0, barca.ocupaLugar("F2A01"));  // Formato incorreto (faltando um dígito) on-point
-        assertEquals(0, barca.ocupaLugar("F02A01")); // off-point
+    public void testaPartDistribuPeso()  {
+        assertEquals(2, barca.ocupaLugar("F30A05"));
+        assertEquals(2, barca.ocupaLugar("F31A10"));
+        assertEquals(2, barca.ocupaLugar("F39A15"));
+        assertEquals(2, barca.ocupaLugar("F45A19"));
     }
 
     @Test
-    public void testOcupado(){
-        assertEquals(1, barca.ocupaLugar("F02A10")); // Assento sendo ocupado
-        assertEquals(1, barca.ocupaLugar("F02A10")); // Assento já ocupado
-        assertEquals(1, barca.ocupaLugar("F02A11")); //off-point
+    public void testaPartAssentoAtribuido()  {
+        assertEquals(3, barca.ocupaLugar("F01A12"));
+        assertEquals(3, barca.ocupaLugar("F19A16"));
+        assertEquals(3, barca.ocupaLugar("F27A04"));
+        assertEquals(3, barca.ocupaLugar("F39A13"));
+    }
+
+    //testes por valor limite
+
+    @Test
+    public void testaValorOffPoint()    {
+        assertEquals(0,barca.ocupaLugar("F01A00"));
+        assertEquals(0,barca.ocupaLugar("F00A01"));
+        assertEquals(0,barca.ocupaLugar("F01A21"));
+        assertEquals(0,barca.ocupaLugar("F61A01"));
     }
 
     @Test
-    public void testDistribuicaoDePeso(){
-        // Não permitido pela distribuição de peso
-        //assertEquals(2, barca.ocupaLugar("F21A01"));
-        assertEquals(2, barca.ocupaLugar("F39A10")); //on-point
-        assertEquals(2, barca.ocupaLugar("F40A01")); //off-point
-        assertEquals(2, barca.ocupaLugar("F60A20"));   
-        //assertEquals(2, barca.ocupaLugar("F60A10"));
+    public void testaValorOnPoint()    {
+        assertEquals(3,barca.ocupaLugar("F01A01"));
+        assertEquals(3,barca.ocupaLugar("F01A20"));
+        assertEquals(3,barca.ocupaLugar("F60A01"));
+        assertEquals(3,barca.ocupaLugar("F60A20"));
     }
-
-    @Test
-    public void sentaCerto(){
-        // Passageiro senta-se na fileira permitida
-        assertEquals(3, barca.ocupaLugar("F02A10")); // Assento já ocupado
-        assertEquals(3, barca.ocupaLugar("F60A10"));
-        assertEquals(3, barca.ocupaLugar("F61A21")); //off-point
-        assertEquals(3, barca.ocupaLugar("F60A20")); //on-point    
-    }
-
-
 
 }
